3841827091eaf57998486dfd995347b48440fdff
Aashikkkkkk <bhattaraiaashik77@gmail.com>
2024-04-05 23:29:33 -0400

    Added the authentication in backend with jwt

--- smart-suggest-server/index.js
+++ smart-suggest-server/index.js
@@ -3,12 +3,12 @@ const cookieParser = require("cookie-parser");
 const db = require("./models");
 const app = express();
 const routes = require("./routes");
-// const authRoutes = require("./routes/authentication.routes.js");
+const authRoutes = require("./routes/authentication.route.js");
 const cors = require("cors");
 const corsOptions = require("./utils/corsOption");
-// const {
-//   validateToken,
-// } = require("./utils/authenticationHandler.js");
+const {
+  validateToken,
+} = require("./utils/authenticationHandler.js");
 require("express-async-errors");
 
 app.use(cors(corsOptions));
@@ -16,8 +16,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use("/auth", authRoutes);
-// app.use(validateToken);
+app.use("/auth", authRoutes);
+app.use(validateToken);
 app.use("/api", routes);
 app.use((error, req, res, next) => {
   console.log(error);
---
+++ smart-suggest-server/routes/authentication.route.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const userController = require("../controllers/user.controller");
+const router = express.Router();
+const loginValidator = require("../validators/login.validator");
+const registrationValidator = require("../validators/registration.validator");
+
+router.post(
+  "/register",
+  registrationValidator,
+  userController.register
+);
+router.post("/login", loginValidator, userController.login);
+router.get("/refresh", userController.refresh);
+router.post("/logout", userController.logout);
+
+module.exports = router;
