70ec136307a1d222c2c9bc9e1e065e11d1d523ce
Aashikkkkkk <bhattaraiaashik77@gmail.com>
2024-04-05 22:58:58 -0400

    Added the CRUD api for items and set up for the backend environment completed

---
+++ smart-suggest-server/validators/create-item.validator.js
@@ -0,0 +1,51 @@
+const { body, validationResult } = require("express-validator");
+
+const itemValidator = [
+  [
+    body("name")
+      .exists()
+      .withMessage("Name is missing")
+      .notEmpty()
+      .withMessage("Name is empty")
+      .isString()
+      .withMessage("Name must be a string"),
+    body("price")
+      .exists()
+      .withMessage("Price is missing")
+      .notEmpty()
+      .withMessage("Price is empty")
+      .isNumeric()
+      .withMessage("Price name must be a number"),
+    body("description")
+      .exists()
+      .withMessage("Description is missing")
+      .notEmpty()
+      .withMessage("Description is empty")
+      .isString()
+      .withMessage("Description must be a string"),
+    body("quantity")
+      .exists()
+      .withMessage("Quantity is missing")
+      .notEmpty()
+      .withMessage("Quantity is empty")
+      .isNumeric()
+      .withMessage("Quantity name must be a number"),
+    body("image_url")
+      .exists()
+      .withMessage("Image URL is missing")
+      .notEmpty()
+      .withMessage("Image URL is empty")
+      .isString()
+      .withMessage("Image URL must be a string"),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ message: "invalid payload", ...errors });
+    } else {
+      next();
+    }
+  },
+];
+
+module.exports = itemValidator;
