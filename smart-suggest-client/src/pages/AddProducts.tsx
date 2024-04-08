5bbbd92cee261d6a9c5cd769e1d57667e6faa0a0
rakshitha-26 <nandureddyc.26@gmail.com>
2024-04-07 20:55:53 -0400

    add product page

--- smart-suggest-client/src/pages/AddProducts.tsx
+++ smart-suggest-client/src/pages/AddProducts.tsx
@@ -7,6 +7,7 @@ import {
   Typography,
   Grid,
 } from '@mui/material';
+
 import Layout from '../layout/Layout';
 
 const AddProductForm = () => {
@@ -18,8 +19,9 @@ const AddProductForm = () => {
     e.preventDefault();
     // Add your logic to handle the form submission here
     // For example, you can create a new product object and add it to a list or store it in localStorage
-    console.log('Product:', { name, price, image });
-    // Reset the form fields
+    // console.log('Product:', { name, price, image });
+    // Reset the form fields of the products
+  
     setName('');
     setPrice('');
     setImage('');
