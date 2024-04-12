8c4244362aa64e6d54bc1abe13597d8bddaf7277
Aashikkkkkk <bhattaraiaashik77@gmail.com>
2024-04-12 00:32:44 -0400

    implemented the functionality for getting the item details in cart

--- smart-suggest-server/index.js
+++ smart-suggest-server/index.js
@@ -28,7 +28,7 @@ app.use((error, req, res, next) => {
 
 app.listen(3000, () => {
   console.log("server is running at http://localhost:3000");
-  db.sequelize.sync({force:true}).then((req) => {
+  db.sequelize.sync().then((req) => {
     console.log("DB synchronized");
   });
 });
--- smart-suggest-server/routes.js
+++ smart-suggest-server/routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const itemRoutes = require("./routes/item.route");
+const cartRoutes = require("./routes/cart.routes")
 
 router.use("/items", itemRoutes);
+router.use("/carts", cartRoutes);
 
 module.exports = router;
